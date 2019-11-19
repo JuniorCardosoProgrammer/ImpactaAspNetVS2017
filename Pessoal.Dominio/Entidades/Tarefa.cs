38e3dd92348b3bed1f3a68d66150e9c8da92c256
Junior Cardoso <joseph.cardoso@live.com>
2019-11-18 22:56:20 -0300

    procedures

---
+++ Pessoal.Dominio/Entidades/Tarefa.cs
@@ -0,0 +1,12 @@
+﻿namespace Pessoal.Dominio.Entidades
+{
+    public class Tarefa
+    {
+        //Página 61 (Apostila 2)
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public Prioridade Prioridade { get; set; }
+        public bool Concluida { get; set; }
+        public string Observacao { get; set; }
+    }
+}
---
+++ Pessoal.Dominio/Interfaces/ITarefaRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using Pessoal.Dominio.Entidades;
+using System.Collections.Generic;
+
+namespace Pessoal.Dominio.Interfaces
+{
+    public interface ITarefaRepositorio
+    {
+        int Inserir(Tarefa tarefa);
+        List<Tarefa> Selecionar();
+        Tarefa Selecionar(int id);
+        void Atualizar(Tarefa tarefa);
+        void Excluir(int id);
+    }
+}
